997abe65349b301ca39bfb7fded091a29a0f21ae
kyangp@gmail.com <kongpyang@stp-er-10-129-78-208.wireless.umn.edu>
2019-09-24 10:02:58 -0500

    updating code

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,7 +1,22 @@
 
 var answerSelected;
+var possibleFriendOne = "";
 
 $("#submitBtn").on("click", function () {
     answerSelected = $("option:selected");
+
+    for (let i = 0; i < friendMatched.length; i++) {
+        for (let j = 0; j < answerSelected.length; j++) {
+            if(possibleFriendOne === "") {
+                possibleFriendOne = friendMatched[i];
+            };
+
+            
+           Math.abs(answerSelected[j].value - friendMatched[i].scores[j]);
+            
+        }
+        const element = friendMatched[i];
+        
+    }
     $("#surveyBody").hide();
 })
